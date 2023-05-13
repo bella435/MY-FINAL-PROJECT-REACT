6b794d3a2f5395fdba61b8820b9b7ca207a74b37
bella435 <mamadou.diallo19@lc.cuny.edu>
2023-05-13 19:09:17 -0400

    new commit my-final-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import BellaDiallo from'./BellaDiallo.jpeg'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Bella
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={BellaDiallo} className="Bella Diallo" alt="logo" />
       </header>
     </div>
   );
 }
 
 export default App;
+
